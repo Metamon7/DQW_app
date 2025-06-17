9cbfbf844846b665861aabd4edef0de4335e860a
h20098@e.hiroshima-cu.ac.jp <>
2025-06-17 10:58:03 +0900

    Update deployment files

--- start.py
+++ start.py
@@ -43,7 +43,7 @@ def callback():
 
     return 'OK', 200
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 8080))
+#    app.run(host="0.0.0.0", port=port)
 
