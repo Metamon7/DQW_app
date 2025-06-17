ee66d6ab4b31e4c745d48bba63ea2a279bdd35c4
h20098@e.hiroshima-cu.ac.jp <>
2025-06-17 09:12:05 +0900

    add requirements.txt and Procfile

--- start.py
+++ start.py
@@ -30,4 +30,4 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 3000)))
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT",  5000)))
