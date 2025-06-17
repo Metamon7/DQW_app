fde29b6d96ae2ff856ff2c5d2da21317ecec9418
h20098@e.hiroshima-cu.ac.jp <>
2025-06-17 10:28:46 +0900

    コードを修正しました

--- start.py
+++ start.py
@@ -42,3 +42,8 @@ def callback():
                 )
 
     return 'OK', 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # RailwayではPORTを環境変数から取る
+    app.run(host="0.0.0.0", port=port)
+
